5eb6f30e42c3087393b124d7437a5b896ae25666
Thibault <tducray@gmail.com>
2022-10-25 14:40:36 +0200

    Added GPL license, updated app icons.

--- PatchOrganizer/PatchOrg.swift
+++ PatchOrganizer/PatchOrg.swift
@@ -5,6 +5,21 @@
 //  Created by Thibault DUCRAY on 19/10/2022.
 //
 
+// This file is part of PatchOrganizer.
+//
+// PatchOrganizer is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// PatchOrganizer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with PatchOrganizer.
+// If not, see <https://www.gnu.org/licenses/>.
+//
+// (c) Copyright 2022 Thibault Ducray
+
 import Foundation
 
 
